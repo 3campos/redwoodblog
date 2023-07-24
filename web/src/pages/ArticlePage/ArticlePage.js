37733d174583d451a046abf912112ce2c7a71c4b
ecampos <ecampos.jr54@gmail.com>
2023-07-24 00:38:00 -0500

    add dynamic rendering for each blog post based on post id

--- web/src/pages/ArticlePage/ArticlePage.js
+++ web/src/pages/ArticlePage/ArticlePage.js
@@ -1,5 +1,6 @@
-import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+
+import ArticleCell from 'src/components/ArticleCell'
 
 const ArticlePage = ({ id }) => {
   // ^id parameter is passed from router
@@ -7,14 +8,7 @@ const ArticlePage = ({ id }) => {
     <>
       <MetaTags title="Article" description="Article page" />
 
-      <h1>ArticlePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/ArticlePage/ArticlePage.js</code>
-      </p>
-      <p>
-        My default route is named <code>article</code>, link to me with `
-      </p>
-      <p>My id is {id}!</p>
+      <ArticleCell id={id} />
     </>
   )
 }
