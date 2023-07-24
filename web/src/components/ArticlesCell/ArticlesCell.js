8c54b3873d2c02bf723c6d1a1e755511cc29eeff
ecampos <ecampos.jr54@gmail.com>
2023-07-24 00:09:02 -0500

    add map function to ArticlesCell component to render article data on home page

--- web/src/components/ArticlesCell/ArticlesCell.js
+++ web/src/components/ArticlesCell/ArticlesCell.js
@@ -2,6 +2,9 @@ export const QUERY = gql`
   query ArticlesQuery {
     articles: posts {
       id
+      title
+      body
+      createdAt
     }
   }
 `
@@ -17,10 +20,16 @@ export const Failure = ({ error }) => (
 
 export const Success = ({ articles }) => {
   return (
-    <ul>
-      {articles.map((item) => {
-        return <li key={item.id}>{JSON.stringify(item)}</li>
-      })}
-    </ul>
+    <>
+      {articles.map((article) => (
+        <article key={article.id}>
+          <header>
+            <h2>{article.title}</h2>
+          </header>
+          <p>{article.body}</p>
+          <div>Posted at: {article.createdAt}</div>
+        </article>
+      ))}
+    </>
   )
 }
