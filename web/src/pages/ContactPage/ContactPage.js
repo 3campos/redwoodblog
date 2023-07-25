ba122c78a098954020ce49f9521b3f92f8d77c1a
ecampos <ecampos.jr54@gmail.com>
2023-07-25 00:06:46 -0500

    add mutation function for creating contact to contactPage

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -6,47 +6,70 @@ import {
   FieldError,
   Label,
 } from '@redwoodjs/forms'
-import { MetaTags } from '@redwoodjs/web'
+import { MetaTags, useMutation } from '@redwoodjs/web'
+
+const CREATE_CONTACT = gql`
+  mutation CreateContactMutation($input: CreateContactInput!) {
+    createContact(input: $input) {
+      id
+    }
+  }
+`
+// The above is a mutation query
 
 const ContactPage = () => {
+  const [create] = useMutation(CREATE_CONTACT)
   const onSubmit = (data) => {
     console.log(data)
+    create({
+      variables: {
+        input: data,
+      },
+    })
   }
+
   return (
     <>
       <MetaTags title="Contact" description="Contact page" />
 
-      <Form onSubmit={onSubmit}>
+      <Form onSubmit={onSubmit} config={{ mode: 'onBlur' }}>
         <Label name="name" errorClassName="error">
           Name
         </Label>
         <TextField
           name="name"
-          errorClassName="error"
           validation={{ required: true }}
+          errorClassName="error"
         />
         <FieldError name="name" className="error" />
 
-        <Label name="email">Email</Label>
+        <Label name="email" errorClassName="error">
+          Email
+        </Label>
         <TextField
           name="email"
-          errorClassName="error"
           validation={{
             required: true,
-            pattern: { value: /^[^@]+@[^.]+\..+$/ },
+            pattern: {
+              value: /^[^@]+@[^.]+\..+$/,
+              message: 'Please enter a valid email address',
+            },
           }}
+          errorClassName="error"
         />
         <FieldError name="email" className="error" />
 
-        <Label name="message">Message</Label>
+        <Label name="message" errorClassName="error">
+          Message
+        </Label>
         <TextAreaField
           name="message"
-          errorClassName="error"
           validation={{ required: true }}
+          errorClassName="error"
         />
         <FieldError name="message" className="error" />
 
-        <Submit>Send Message</Submit>
+        <Submit>Save</Submit>
       </Form>
     </>
   )
