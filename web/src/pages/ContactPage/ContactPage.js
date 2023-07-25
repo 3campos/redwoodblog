a9b341728d202ab569ab10967669a4ed3dde2a93
ecampos <ecampos.jr54@gmail.com>
2023-07-24 23:29:48 -0500

    add submit button and function to form

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -2,11 +2,14 @@ import { Form, TextField, TextAreaField, Submit } from '@redwoodjs/forms'
 import { MetaTags } from '@redwoodjs/web'
 
 const ContactPage = () => {
+  const onSubmit = (data) => {
+    console.log(data)
+  }
   return (
     <>
       <MetaTags title="Contact" description="Contact page" />
 
-      <Form>
+      <Form onSubmit={onSubmit}>
         <label>Name</label>
         <TextField name="name" />
 
