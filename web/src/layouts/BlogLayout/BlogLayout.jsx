1b3ac2a1611e8f2995b529646bcbebe6298d1acf
ecampos <ecampos.jr54@gmail.com>
2023-08-14 17:44:24 -0500

    show user email address on BlogLayout when logged in; else, login button is shown

--- web/src/layouts/BlogLayout/BlogLayout.jsx
+++ web/src/layouts/BlogLayout/BlogLayout.jsx
@@ -7,9 +7,22 @@ const BlogLayout = ({ children }) => {
   return (
     <>
       <header>
-        <h1>
-          <Link to={routes.home()}>Redwood Blog</Link>
-        </h1>
+        <div className="flex-between">
+          <h1>
+            <Link to={routes.home()}>Redwood Blog</Link>
+          </h1>
+
+          {isAuthenticated ? (
+            <div>
+              <span>Logged in as {currentUser.email}</span>{' '}
+              <button type="button" onClick={logOut}>
+                Logout
+              </button>
+            </div>
+          ) : (
+            <Link to={routes.login()}>Login</Link>
+          )}
+        </div>
         <nav>
           <ul>
             <li>
