b2ba7c5179db75bb6b5170154e6f42034fb45d0f
ecampos <ecampos.jr54@gmail.com>
2023-08-24 00:15:11 -0500

    fix null text on bloglayout component

--- web/src/layouts/BlogLayout/BlogLayout.jsx
+++ web/src/layouts/BlogLayout/BlogLayout.jsx
@@ -14,7 +14,7 @@ const BlogLayout = ({ children }) => {
 
           {isAuthenticated ? (
             <div>
-              <span>Logged in as {currentUser.email}</span>{' '}
+              <span>Logged in as {currentUser.email}</span>
               <button type="button" onClick={logOut}>
                 Logout
               </button>
